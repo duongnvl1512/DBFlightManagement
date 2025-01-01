3c0ac9b1f5f54900798f91a70968ccfca4916cf7
Quốc Bảo <baonq21@uef.edu.vn>
2025-01-01 14:52:50 +0700

    Thêm phần cs của Introduction

---
+++ DBFlightManagement/Pages/Introduction.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DBFlightManagement.Pages
+{
+    public class IntroductionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
